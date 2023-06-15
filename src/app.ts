aabf27eec8c6ef5d450357deebd3d42095026dce
Shivdeep Singh <appnox@Shivdeeps-MacBook-Pro.local>
2023-06-15 19:43:17 +0530

    some changes done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import http from 'http';
-import expressServer from './server';
-import connectPSQlDb from './config/dbConnection';
-import { Server, Socket } from 'socket.io';
-import { socketHandler } from './utils/socketHandler';
+import http from "http";
+import expressServer from "./server";
+import connectPSQlDb from "./config/dbConnection";
+import { Server, Socket } from "socket.io";
+import { socketHandler } from "./utils/socketHandler";
 
 // Normalize port number which will expose server
 const port = normalizePort(5000);
@@ -11,32 +11,31 @@ const port = normalizePort(5000);
 const expressInstance = new expressServer().expressInstance;
 
 // Make port available within server
-expressInstance.set('port', port);
+expressInstance.set("port", port);
 
 // Create the HTTP Express Server
 const server = http.createServer(expressInstance);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:8082',
-    methods: ['GET', 'POST'],
+    origin: "*",
+    methods: ["GET", "POST"],
   },
 });
 
-var name:String;
+var name: String;
 // Listen for when the client connects via socket.io-client
 
-
-// Start listening on the specified Port (Default: 3000)
+// Start listening on the specified Port (Default: 5000)
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
-socketHandler(io)
+socketHandler(io);
 connectPSQlDb();
 
 // Port Normalization
 function normalizePort(val: number | string): number | string | boolean {
-  const port: number = typeof val === 'string' ? parseInt(val, 10) : val;
+  const port: number = typeof val === "string" ? parseInt(val, 10) : val;
   if (isNaN(port)) {
     return val;
   } else if (port >= 0) {

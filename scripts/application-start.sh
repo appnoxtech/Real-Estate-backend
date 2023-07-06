ce94ba129700c7c7775aca033cd2037f1ea3de7b
manjari-appnox <133857831+manjari-appnox@users.noreply.github.com>
2023-07-06 18:32:41 +0530

    Update application-start.sh

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd /home/ubuntu
+
+# Stop and remove any existing containers
+sudo docker compose down
+
+# Remove all unused containers, images, networks, and volumes
+sudo docker system prune --all --force
+
+# Build and start the Docker containers
 sudo docker compose up -d --build

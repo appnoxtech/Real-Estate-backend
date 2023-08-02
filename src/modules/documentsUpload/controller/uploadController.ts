7e3687d7a2b7b281ecc34eee4a5e7f5b83f6ffe6
AnchalPal01 <mac@Macs-MacBook-Pro.local>
2023-08-02 18:47:53 +0530

    fixing issue

--- src/modules/documentsUpload/controller/uploadController.ts
+++ src/modules/documentsUpload/controller/uploadController.ts
@@ -2,14 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { FileUploadService } from "../services/uploadService";
 import { respHndlr} from "../../../utils/index"
 import { RESPONSE_STATUS } from "../../../utils/constants";
+import { logger } from "../../../utils/logger";
 
 const FileUploadServiceInstance = new FileUploadService()
-
+logger.info("check---->02")
 export class FileUploadController {
 
     async fileupload(req: Request, res: Response) {
         try {
             const data = await FileUploadServiceInstance.DocumentUpload(req);
+            logger.info("data",data)
             respHndlr.sendSuccess(res, data, RESPONSE_STATUS.SUCCESS_CREATED);
         } catch (err: any) {
             respHndlr.sendError(res, err);
